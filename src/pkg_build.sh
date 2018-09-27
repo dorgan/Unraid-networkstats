9ea1d72b8c746390a35d4deb030fd11587309bd3
Donald Organ <dorgan@donaldorgan.com>
2018-09-27 19:00:19 -0400

    Tweaking pkg script

--- src/pkg_build.sh
+++ src/pkg_build.sh
@@ -16,6 +16,7 @@ else
 
     for x in '' a b c d e d f g h i j k l m n o p q r s t u v w x y z; do
         PKG="${ARCHIVE}/${PLUGIN}-${VERSION}${x}${ARCH}.txz"
+        PRINT "Looking for  ${PKG}"
         if [[ ! -f $PKG ]]; then
           PACKAGE=$PKG
           VERSION="${VERSION}${x}"
