acc0d10fbc6a5887dfa30c3d5817ded73e28fb3b
Donald Organ <dorgan@donaldorgan.com>
2018-09-29 19:56:26 -0400

    Tweaking

--- src/networkstats/usr/local/emhttp/plugins/networkstats/service.php
+++ src/networkstats/usr/local/emhttp/plugins/networkstats/service.php
@@ -5,11 +5,11 @@
         } else if ($_GET['action'] === 'stop') {
             shellexec('/etc/init.d/vnstat stop');
         }
-        $status = ""
+        $active       = (intval(trim(shell_exec( "[ -f /proc/`cat /var/run/vnstat/vnstat.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ))) === 1);
         if ($active) {
             $status = "<span class='green' id="serviceStatus">Running</span> <a href="/plugins/' . $plugin .'/service.php?$action=stop" class="xhr">stop</a>";
         } else {
-            $status= "<span class='orange' id="serviceStatus">Stopped</span> <a href="/plugins/' . $plugin .'/service.php?$action=stop" class="xhr">start</a>";
+            $status= "<span class='orange' id="serviceStatus">Stopped</span> <a href="/plugins/' . $plugin .'/service.php?$action=start" class="xhr">start</a>";
         }
 
         echo($status);
