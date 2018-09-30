2019ceffeab97ce8a3f833868a8e06b4a4cb8f14
dorgan@donaldorgan.com <dorgan@donaldorgan.com>
2018-09-29 20:17:02 -0400

    v2018.09.29c
    - Add ability for user to start/stop vnstat
    - Fix plugins ability to to detect status of vnstat daemon

---
+++ src/networkstats/usr/local/emhttp/plugins/networkstats/service.php
@@ -0,0 +1,19 @@
+<?php
+    $plugin = 'networkstats';
+
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] === 'start') {
+            shell_exec('/etc/rc.d/rc.vnstat start');
+        } else if ($_GET['action'] === 'stop') {
+            shell_exec('/etc/rc.d/rc.vnstat stop');
+        }
+        $active       = (intval(trim(shell_exec( "[ -f /proc/`cat /var/run/vnstat/vnstat.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ))) === 1);
+        if ($active) {
+            $status = '<span class="green" id="serviceStatus">Running <a href="/plugins/' . $plugin .'/service.php?action=stop" class="xhr">stop</a></span>';
+        } else {
+            $status= '<span class="orange" id="serviceStatus">Stopped <a href="/plugins/' . $plugin .'/service.php?action=start" class="xhr">start</a></span>';
+        }
+
+        echo($status);
+    }
+?>
