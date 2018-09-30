637cbd8400a3932e11a3ef2e19e53997eec5cea6
Donald Organ <dorgan@donaldorgan.com>
2018-09-29 20:10:45 -0400

    Tweaks

--- src/networkstats/usr/local/emhttp/plugins/networkstats/service.php
+++ src/networkstats/usr/local/emhttp/plugins/networkstats/service.php
@@ -1,15 +1,17 @@
 <?php
+    $plugin = 'networkstats';
+
     if (isset($_GET['action'])) {
         if ($_GET['action'] === 'start') {
-            shellexec('/etc/init.d/vnstat start');
+            shell_exec('/etc/rc.d/rc.vnstat start');
         } else if ($_GET['action'] === 'stop') {
-            shellexec('/etc/init.d/vnstat stop');
+            shell_exec('/etc/rc.d/rc.vnstat stop');
         }
         $active       = (intval(trim(shell_exec( "[ -f /proc/`cat /var/run/vnstat/vnstat.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ))) === 1);
         if ($active) {
-            $status = "<span class='green' id="serviceStatus">Running</span> <a href="/plugins/' . $plugin .'/service.php?$action=stop" class="xhr">stop</a>";
+            $status = '<span class="green" id="serviceStatus">Running <a href="/plugins/' . $plugin .'/service.php?action=stop" class="xhr">stop</a></span>';
         } else {
-            $status= "<span class='orange' id="serviceStatus">Stopped</span> <a href="/plugins/' . $plugin .'/service.php?$action=start" class="xhr">start</a>";
+            $status= '<span class="orange" id="serviceStatus">Stopped <a href="/plugins/' . $plugin .'/service.php?action=start" class="xhr">start</a></span>';
         }
 
         echo($status);
