ac84c406b53494b2565dbbb54058ef9bce1763ed
dorgan@donaldorgan.com <dorgan@donaldorgan.com>
2019-02-11 19:35:22 -0500

    Update vnstat.php

--- src/networkstats/usr/local/emhttp/plugins/networkstats/vnstat.php
+++ src/networkstats/usr/local/emhttp/plugins/networkstats/vnstat.php
@@ -3,7 +3,7 @@ $file = tmpfile();
 $name = stream_get_meta_data($file);
 $cmd = 'vnstati' . (!isset($_GET['s']) ? ' --style ' . $_GET['style'] . ' -' . $_GET['dh']  : ' -s ') . ' -i ' . $_GET['i'] . ' -o ' .$name['uri'];
 $output = shell_exec($cmd);
-$size = filesize($file);
+$size = filesize(stream_get_meta_data($file)['uri']);
 if (!isset($_GET['debug'])) {
   header('Content-Type: image/png');
   header('Content-Length: ' .$size );
