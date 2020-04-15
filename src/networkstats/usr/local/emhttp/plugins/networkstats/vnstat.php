3e41bda0f1215a866f4efd79c1785c8e4a82d707
Donald Organ <dorgan@donaldorgan.com>
2020-04-14 20:39:57 -0400

    2020.04.14c

--- src/networkstats/usr/local/emhttp/plugins/networkstats/vnstat.php
+++ src/networkstats/usr/local/emhttp/plugins/networkstats/vnstat.php
@@ -1,7 +1,7 @@
 <?php
 $file = tmpfile();
 $name = stream_get_meta_data($file);
-$cmd = 'vnstati' . (!isset($_GET['s']) ? ' --style ' . $_GET['style'] . ' -' . $_GET['dh']  : ' -s ') . ' -i ' . $_GET['i'] . ' -o ' .$name['uri'];
+$cmd = 'vnstati' . (!isset($_GET['s']) ? ' --style ' . $_GET['style'] . ' -' . $_GET['dh']  : ' -s ') . ' -i ' . $_GET['i'] . ' -o ' .$name['uri'] . (isset($_GET['header']) ? ' --headertext "' . urldecode($_GET['header']) . '"' : '');
 $output = shell_exec($cmd);
 $size = filesize(stream_get_meta_data($file)['uri']);
 if (!isset($_GET['debug'])) {
